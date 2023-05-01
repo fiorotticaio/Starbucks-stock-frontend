aa039b6e14d08ad4c0df8137f682d5587e968625
Matheus <mt.schreiber01@gmail.com>
2023-05-01 20:40:48 -0300

    :rocket: connecting to java kafka

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,35 @@ import Product from "@/components/Product";
 
 import mediumCupImg from "@/public/mediumCup.png";
 import largeCupImg from "@/public/largeCup.png";
+import { useEffect, useState } from "react";
+import api from "@/services/api";
 
 export default function Home() {
+
+  const [ mediumPrice, setMediumPrice ] = useState<number>(0);
+  const [ largePrice, setLargePrice ] = useState<number>(0);
+
+  async function fetchKafka() {
+    const response = await api.get('/price/all')
+    console.log(response.data)
+    
+    if (response.data) {
+      setMediumPrice(parseFloat(response.data))
+      setLargePrice(parseFloat(response.data))
+    }
+  }
+
+  useEffect(()=>{
+    setInterval(fetchKafka, 1000)
+  },[])
+
   return (
     <div className={styles.home}>
       <Header />
       <div className={styles.banner} />
       <div className={styles.content}>
         <section className={styles.contentTitle}>
-          <h1>SHOP</h1>
+          <h1 onClick={fetchKafka}>SHOP</h1>
           <h5>
             From the beginning, starbucks set out to be a different kind of
             company. One that not only celebrated coffee but also connection.
@@ -25,13 +45,13 @@ export default function Home() {
         <section className={styles.contentProducts}>
           <Product
             image={mediumCupImg}
-            price={2.5}
+            price={mediumPrice}
             name="Medium Cup"
             size="250ml"
           />
           <Product
             image={largeCupImg}
-            price={4.5}
+            price={largePrice}
             name="Large Cup"
             size="400ml"
           />

3c01478ec25cccedc03834b2412c841e3b9a368e
Matheus <mt.schreiber01@gmail.com>
2023-05-07 13:47:34 -0300

    :whale: docker implementation

--- services/api.tsx
+++ services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: `http://${process.env.NEXT_PUBLIC_APIHOST}:${process.env.NEXT_PUBLIC_APIPORT}`
 })
 
 export default api;
